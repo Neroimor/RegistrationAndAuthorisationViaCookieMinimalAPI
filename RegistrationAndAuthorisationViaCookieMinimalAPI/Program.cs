f65712119464a325aba082e7917cc5f95c9b443b
Neroimor <neroimor@gmail.com>
2025-07-09 09:46:37 +0300

    added conection to database and added migrations

--- RegistrationAndAuthorisationViaCookieMinimalAPI/Program.cs
+++ RegistrationAndAuthorisationViaCookieMinimalAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RegistrationAndAuthorisationViaCookieMinimalAPI.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddLogging();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
