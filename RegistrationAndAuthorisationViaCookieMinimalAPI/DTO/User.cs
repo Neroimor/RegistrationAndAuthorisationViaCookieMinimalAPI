4760c6666b70fe7275ddd070f1dd7659c78ef0ef
Neroimor <neroimor@gmail.com>
2025-07-09 09:40:41 +0300

    Database connection configured, DTO and Response/Request classes added

---
+++ RegistrationAndAuthorisationViaCookieMinimalAPI/DTO/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistrationAndAuthorisationViaCookieMinimalAPI.DTO
+{
+    public record class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+
+        [RegularExpression(@"^[A-Za-z!@#$%^&*()_\-+=\[{\]};:'"",<.>/?\\|`~]+$")]
+        public string? Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
---
+++ RegistrationAndAuthorisationViaCookieMinimalAPI/DataBase/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrationAndAuthorisationViaCookieMinimalAPI.DTO;
+
+namespace RegistrationAndAuthorisationViaCookieMinimalAPI.DataBase
+{
+    public class AppDBContext : DbContext
+    {
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users_Data { get; set; }
+
+    }
+}
